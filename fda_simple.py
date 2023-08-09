1e1142f8986ccd8b92c333d82dfbc439d0a860c8
Fred Chasin <fchasin52@gmail.com>
2023-08-09 09:35:05 -0700

    add usage clarification and refactoring

--- fda_simple.py
+++ fda_simple.py
@@ -2,8 +2,9 @@ import csv
 
 import logging
 import json
+from fda import write, write_first, write_next
 
-def doit():
+def from_json():
     count = 0
     with open('device-510k-0001-of-0001.json', encoding='utf-8') as inputfile:
         file = json.load(inputfile)
@@ -19,28 +20,7 @@ def doit():
             app.pop('openfda')
         write_first(apps)
 
-def write_first(apps):
-    csvFile = open("filename.csv", 'w')
-    csvWriter = csv.writer(csvFile)
-    # write header
-    keys = apps[0].keys()
-    csvWriter.writerow(keys)
-
-    # write data
-    write(apps, csvWriter)
-    csvFile.close()
-
-def write_next(apps):
-    csvFile = open("filename.csv", 'a')
-    csvWriter = csv.writer(csvFile)
-    write(apps, csvWriter)
-    csvFile.close()
-
-def write(apps, csvWriter):
-    for line in apps:
-        csvWriter.writerow(line.values())
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', level=logging.DEBUG)
-    # get()
-    doit()
+    from_json()
